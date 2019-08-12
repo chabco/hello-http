3727d8f322104fcdf0ef2aae0438f96cf8f05863
charlescortez <charlesmph@gmail.com>
2019-08-12 15:24:12 -0400

    Added nodemon

---
+++ index.js
@@ -0,0 +1,25 @@
+// Without "./", Node looks in its global module folder
+const http = require('http');
+function handleReqAndRes(req, res) {
+    console.log(req.url);
+    const timestamp = new Date();
+    console.log("I totally recieved a request!")
+    res.end(`<h1>Hello World ${timestamp}</h1>
+    <p>lol wtf</p>`);
+    //.end() sends the response.. "Tapes up the box and ships it.""
+};
+
+// What's a server??
+// A piece of code or a computer running a piece of code that does something useful for YOU.
+// Usually servers store data and often manipulate that data.
+
+const server = http.createServer(handleReqAndRes);
+// http.createServer() accepts a function and returns a "server".
+// When the server receives a Request:
+// 1. It puts together *most* of a Response.
+// 2. It calls *your* function and passes it the Request and Response.
+
+
+
+// What is 3000?
+server.listen(3000);

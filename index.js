62727ea8842580e9d321de7e744337c9c3f4405e
charlescortez <charlesmph@gmail.com>
2019-08-12 16:09:42 -0400

    Added a short madlib game that features 3 random characters

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ function handleReqAndRes(req, res) {
     // console.log("I totally recieved a request!")
     
     
-    let singleHP = nerds.resolve('Harry Potter').asArray()[0].first;
-    console.log(singleHP);
+    let singleHP1 = nerds.resolve('Harry Potter').asArray()[0].first;
+    console.log(singleHP1);
+    let singleHP2 = nerds.resolve('Harry Potter').asArray()[0].last;
+    console.log(singleHP2);
+    
+    let genSW1 = nerds.resolve('Star Wars').asArray()[0].name;
+    console.log(genSW1);
+
+    let genSW2 = nerds.resolve('Star Wars').asArray()[0].name;
+    console.log(genSW2);
 
 
     //.end() sends the response.. "Tapes up the box and ships it.""
     // res.end(`<h1>Hello World ${timestamp}</h1>
     // <p>lol wtf</p>`);
-    res.end(singleHP);
+    res.end(`<h1><font color="lightpink">${singleHP1} ${singleHP2}</font> fell into the wrong universe and found themselves in middle the city where <font color="lightpink">${genSW1}</font> and <font color="lightpink">${genSW2}</font> reside! To <font color="lightpink">${genSW2}'s</font> surprise, <font color="lightpink">${genSW1}</font> ran away in fear due to feeling a different power coming from <font color="lightpink">${singleHP1} ${singleHP2}</font>. "HELP!" <font color="lightpink">${genSW2}</font> cried. Confused, <font color="lightpink">${singleHP1} ${singleHP2}</font> became enraged and started to fly far into the sky...</h1>`);
 };
 
 // What's a server??
